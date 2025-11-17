66a65e00ea31eb2cb149a0d925e67e53a9df1f34
Pedro Otávio de Oliveira Martins <pedromartins06072008@gmail.com>
2025-11-17 11:02:32 -0300

    criação do java

--- cadastrarFilme.js
+++ cadastrarFilme.js
@@ -1,23 +1,24 @@
-document.getElementById('formCliente').addEventListener('submit', async function (e) {
+document.getElementById('formFilme').addEventListener('submit', async function (e) {
     e.preventDefault();
 
-    const cpf = document.getElementById('cpf').value;
     const nome = document.getElementById('nome').value;
+    const genero = document.getElementById('genero').value;
+    const duracao = document.getElementById('duracao').value;
+    const classificacao = document.getElementById('classificacao').value;
+    const ano = document.getElementById('ano').value;  
 
-    const response = await fetch('http://localhost:3000/cliente', {
+    const response = await fetch('http://localhost:3000/cadastroFilme', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ cpf, nome })
+        body: JSON.stringify({ nome, genero, duracao, classificacao, ano })
     });
 
     const data = await response.json();
 
     if (response.ok) {
-        document.getElementById('message').textContent = 'Cliente cadastrado!';
-        document.getElementById('formCliente').reset();
+        document.getElementById('message').textContent = 'Filme cadastrado!';
+        document.getElementById('formFilme').reset();
     } else {
         document.getElementById('message').textContent = 'Erro: ' + data.error;
     }
 });
-
-
